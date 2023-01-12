d7d94a687f101a9777ad9f932a66502cc1e3c720
trygve <git@woldseth.xyz>
2023-01-12 13:24:39 +0100

    fixed the java part

---
+++ java_feature_gen/src/main/java/no/tryxelindustries/java_feature_gen/Main.java
@@ -0,0 +1,11 @@
+package no.tryxelindustries.java_feature_gen;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("lalalalala");
+    }
+
+    public static void genJavaFeatures(){
+
+    }
+}
