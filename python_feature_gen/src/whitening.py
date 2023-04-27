75e0fa6f06c8d5105c0f285d0149061b3e19037f
Axel <acluiggi@stud.ntnu.no>
2023-04-27 13:02:20 +0200

    whitenignei

--- python_feature_gen/src/whitening.py
+++ python_feature_gen/src/whitening.py
@@ -2,17 +2,17 @@ from typing import List
 
 import numpy as np
 
-from entitys import EmbeddedArticle
+from entitys import EmbeddedArticle, NewsArticle
 
 
 class whitener:
     def __init__(self, num_dims):
-        self.covariance_matrix = np.identity(num_dims)
+        self.covariance_matrix = np.zeros((num_dims, num_dims))
         self.mu_vec = np.zeros(num_dims)
         self.n = 0
 
     def update(self, embedded_articles: List[EmbeddedArticle]):
-        embeddings = np.array([x for x in [y.embeddings for y in embedded_articles for j in j]])
+        embeddings = np.array([x for x in [j for y in embedded_articles for j in y.embeddings]])
         new_n = len(embeddings)
         new_mu = np.mean(embeddings, axis=0, keepdims=True)
         new_cov = np.cov(embeddings.T)
@@ -23,7 +23,7 @@ class whitener:
 
     #NB: antar at alle embedded articles allerede har blitt kjørt gjennom update!
     def whiten_embeddings(self, embedded_articles: List[EmbeddedArticle], desired_dims: int = None) -> np.array:
-        embeddings = np.array([x for x in [y.embeddings for y in embedded_articles for j in j]])
+        embeddings = np.array([x for x in [j for y in embedded_articles for j in y.embeddings]])
         desired_dims = embeddings.shape[1] if (
                 desired_dims is None or desired_dims < 0 or desired_dims > embeddings.shape[1]) else desired_dims
         print(desired_dims)
@@ -32,5 +32,5 @@ class whitener:
         u, s, vh = np.linalg.svd(self.covariance_matrix)
         w = np.dot(u, np.diag(1 / np.sqrt(s)))
         op = w.T @ self.covariance_matrix @ w
-        print(len(op[op > 0.9]))
+        # print(len(op[op > 0.9]))
         return (embeddings - self.mu_vec) @ w[:, :desired_dims]
